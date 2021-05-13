d0ebb45495583170004c553f275c13102e48869f
nasso <nassomails@gmail.com>
2021-05-13 22:02:09 +0200

    Separate OpenGL backend in its own file

---
+++ ige/src/plugin/renderer/backend.hpp
@@ -0,0 +1,17 @@
+#ifndef EA0C3563_AB58_4A44_900B_8D737ECE36A0
+#define EA0C3563_AB58_4A44_900B_8D737ECE36A0
+
+#include "ige/ecs/World.hpp"
+
+namespace ige {
+namespace renderer {
+    namespace backend {
+
+        void clear_buffers(ecs::World&);
+        void render_meshes(ecs::World&);
+
+    }
+}
+}
+
+#endif /* EA0C3563_AB58_4A44_900B_8D737ECE36A0 */
