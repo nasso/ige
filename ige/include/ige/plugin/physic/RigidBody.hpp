4d5f3c3ee8b63a611557cc644498b75d25c37db0
Arcarox <arcarox@localhost.localdomain>
2021-05-30 15:54:41 +0200

    Add some properties to rigidbody

--- ige/include/ige/plugin/physic/RigidBody.hpp
+++ ige/include/ige/plugin/physic/RigidBody.hpp
@@ -22,19 +22,40 @@ public:
     RigidBody& is_kinematic(bool is_kinematic);
     bool is_kinematic() const;
 
-    RigidBody& set_mass(float mass);
-    float get_mass() const;
+    void mass(float mass);
+    float mass() const;
+
+    bool use_gravity() const;
+    void use_gravity(bool use_gravity);
 
     RigidBody& apply_force(glm::vec3 force);
     const std::vector<glm::vec3>& get_forces() const;
     void clear_forces();
 
+    void velocity(glm::vec3 velocity);
+    glm::vec3 velocity() const;
+
     void update();
+
+    bool freeze_rotation() const;
+    void freeze_rotation(bool free_rotation);
+
+    bool freeze_position() const;
+    void freeze_position(bool free_position);
+
+    glm::vec3 center_of_mass() const;
+    void center_of_mass(glm::vec3 center_of_mass);
+    void reset_center_of_mass();
 
 private:
     std::vector<glm::vec3> m_forces;
+    glm::vec3 m_center_of_mass = glm::vec3 { 0.f };
+    glm::vec3 m_velocity = glm::vec3 { 0.f };
     float m_mass;
     bool m_is_kinematic;
+    bool m_use_gravity = false;
+    bool m_freeze_rotation = false;
+    bool m_freeze_position = false;
 
     RigidBodyStatus m_status;
     Collider m_collider;
