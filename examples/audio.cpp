d00364976278b28e106231fc7c251561b4f93d07
nasso <nassomails@gmail.com>
2021-06-10 23:50:56 +0200

    Cleanup example code

--- examples/audio.cpp
+++ examples/audio.cpp
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include <thread>
 
-using namespace std::literals::chrono_literals;
 using ige::asset::Material;
 using ige::asset::Mesh;
 using ige::asset::Texture;
 using ige::core::App;
 using ige::core::EventChannel;
 using ige::core::State;
+using ige::ecs::EntityId;
 using ige::ecs::Schedule;
 using ige::plugin::audio::AudioClip;
 using ige::plugin::audio::AudioEngine;
@@ -22,35 +22,39 @@ using ige::plugin::transform::Transform;
 using ige::plugin::transform::TransformPlugin;
 
 class RootState : public State {
+    std::optional<EntityId> source_entity;
 
+public:
     void on_start(App& app) override
     {
         std::shared_ptr<AudioClip> clip(
             new AudioClip("./assets/waves_mono.ogg"));
 
         // Warning: Only mono sound files support 3D Spatialization
-        auto demo_ent = app.world().create_entity(
-            AudioSource {}, Transform::from_pos(glm::vec3(500.0f, 0.0f, 0.0f)));
-        app.world().get_component<AudioSource>(demo_ent)->load_clip(clip);
-        app.world().get_component<AudioSource>(demo_ent)->play();
+        source_entity = app.world().create_entity(
+            Transform::from_pos({ 500.0f, 0.0f, 0.0f }));
 
-        // Warning: A world can only contain one AudioListener !
-        auto listener = app.world().create_entity(AudioListener {});
-        app.world().add_component<Transform>(
-            listener, Transform::from_pos(glm::vec3(0.0f, 0.0f, 0.0f)));
+        auto& source
+            = app.world().emplace_component<AudioSource>(*source_entity);
+        source.load_clip(clip);
+        source.play();
+
+        // Warning: A world can only contain one AudioListener!
+        auto listener
+            = app.world().create_entity(AudioListener {}, Transform {});
     }
 
     void on_update(App& app) override
     {
-        static size_t count = 0;
-        auto listeners = app.world().query<AudioListener, Transform>();
-        auto source = app.world().query<AudioSource, Transform>();
+        auto source = app.world().get_component<AudioSource>(*source_entity);
 
-        if (!std::get<1>(source[0]).is_playing()) {
+        if (!source->is_playing()) {
             app.quit();
         }
+
+        auto listeners = app.world().query<AudioListener, Transform>();
         for (auto& [entity, listener, xform] : listeners) {
-            xform.translate(glm::vec3(1, 0, 0));
+            xform.translate({ 1.0f, 0.0f, 0.0f });
             std::cout << "New listener position:"
                       << " X=" << xform.translation().x
                       << " Y=" << xform.translation().y
@@ -59,24 +63,32 @@ class RootState : public State {
 
         // This is just to slow the process down a bit, don't use that in a real
         // use case
-        std::this_thread::sleep_for(10ms);
+        {
+            using namespace std::literals::chrono_literals;
+
+            std::this_thread::sleep_for(10ms);
+        }
     }
 };
 
 int main()
 {
-    std::string holder;
-
     std::cout
         << "[Audio Example] This example loads a test OGG file and performs 3D "
            "Spatialization using a source and a listener. You should be able "
            "to hear the sound go from right to left (You need headphones). The "
-           "switch should happen at X=500.\n\nPress [Enter] when you're ready"
+           "switch should happen at X=500.\n"
+           "\n"
+           "Press [Enter] when you're ready"
         << std::endl;
-    std::getline(std::cin, holder);
+
+    {
+        std::string str;
+        std::getline(std::cin, str);
+    }
+
     App::Builder()
         .add_plugin(TransformPlugin {})
         .add_plugin(AudioPlugin {})
         .run<RootState>();
-    return (0);
 }
