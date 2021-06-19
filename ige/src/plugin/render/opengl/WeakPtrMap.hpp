eb88b3316d9cb44ff5064660d7f1fc6cd933d452
nasso <nassomails@gmail.com>
2021-06-19 20:37:26 +0200

    Fix invalid WeakPtrMap implementation

--- ige/src/plugin/render/opengl/WeakPtrMap.hpp
+++ ige/src/plugin/render/opengl/WeakPtrMap.hpp
@@ -5,54 +5,84 @@
 #include <functional>
 #include <memory>
 #include <unordered_map>
+#include <utility>
 
 namespace detail {
 
 template <typename T>
-struct WeakPtrHash {
-    std::size_t operator()(const std::weak_ptr<T>& ptr) const
+struct SharedPtrHash {
+    std::size_t operator()(const std::shared_ptr<T>& ptr) const
     {
-        std::size_t res = 17;
-        res = res * 31 + std::hash<bool>()(ptr.expired());
-        res = res * 31 + std::hash<T*>()(ptr.lock().get());
-        return res;
+        return std::hash<T*> {}(ptr.get());
     }
 };
 
 template <typename T>
-struct WeakPtrEq {
-    bool
-    operator()(const std::weak_ptr<T>& lhs, const std::weak_ptr<T>& rhs) const
+struct SharedPtrEq {
+    bool operator()(
+        const std::shared_ptr<T>& lhs, const std::shared_ptr<T>& rhs) const
     {
-        if (lhs.expired() || rhs.expired()) {
-            return lhs.expired() && rhs.expired();
-        }
-
-        return lhs.lock().get() == rhs.lock().get();
+        return lhs.get() == rhs.get();
     }
 };
 
 }
 
 template <typename K, typename V>
-class WeakPtrMap
-    : public std::unordered_map<
-          std::weak_ptr<K>, V, detail::WeakPtrHash<K>, detail::WeakPtrEq<K>> {
+class WeakPtrMap {
+    using Map = std::unordered_map<
+        std::shared_ptr<K>, V, detail::SharedPtrHash<K>,
+        detail::SharedPtrEq<K>>;
+
 public:
+    using SharedKey = std::shared_ptr<K>;
+    using Iterator = Map::iterator;
+    using ConstIterator = Map::const_iterator;
+
     void clean(bool force = false)
     {
         if (!force) {
-            const float lf = static_cast<float>(this->size() + 1)
-                / static_cast<float>(this->bucket_count());
+            const float lf = static_cast<float>(m_map.size() + 1)
+                / static_cast<float>(m_map.bucket_count());
 
-            force = lf >= this->max_load_factor();
+            force = lf >= m_map.max_load_factor();
         }
 
         if (force) {
-            std::erase_if(
-                *this, [](const auto& item) { return item.first.expired(); });
+            std::erase_if(m_map, [](const auto& item) {
+                return item.first.use_count() == 1;
+            });
         }
     }
+
+    Iterator end()
+    {
+        return m_map.end();
+    }
+
+    ConstIterator end() const
+    {
+        return m_map.end();
+    }
+
+    Iterator find(const SharedKey& k)
+    {
+        return m_map.find(k);
+    }
+
+    ConstIterator find(const SharedKey& k) const
+    {
+        return m_map.find(k);
+    }
+
+    template <typename... Args>
+    std::pair<Iterator, bool> emplace(Args&&... args)
+    {
+        return m_map.emplace(std::forward<Args>(args)...);
+    }
+
+private:
+    Map m_map;
 };
 
 #endif /* A33C094A_DF9B_4BBB_B3B2_5AA3DD54CB65 */
