9440124fa063fbaedbf59626ebe6fbe1697e4264
nasso <nassomails@gmail.com>
2021-07-11 20:15:13 +0200

    Cleanup fx-gltf package definition

--- xmake-repo/packages/f/fx-gltf/xmake.lua
+++ xmake-repo/packages/f/fx-gltf/xmake.lua
@@ -1,11 +1,13 @@
 package("fx-gltf")
-    set_homepage("https://github.com/jessey-git/fx-gltf/")
+    set_homepage("https://github.com/jessey-git/fx-gltf")
     set_description("A C++14/C++17 header-only library for simple, efficient, and robust serialization/deserialization of glTF 2.0")
     set_license("MIT")
 
-    set_urls("https://github.com/jessey-git/fx-gltf/archive/refs/tags/v$(version).tar.gz")
+    add_urls("https://github.com/jessey-git/fx-gltf/archive/refs/tags/v$(version).tar.gz", {alias = "archive"})
+    add_urls("https://github.com/jessey-git/fx-gltf.git", {alias = "github"})
 
-    add_versions("1.2.0", "d8eaceba72ea6574b982c7b0d2328fd3f8ad519db4a37cf63cd3f8020d7722bf")
+    add_versions("github:1.2.0", "v1.2.0")
+    add_versions("archive:1.2.0", "d8eaceba72ea6574b982c7b0d2328fd3f8ad519db4a37cf63cd3f8020d7722bf")
 
     add_deps("nlohmann_json")
 
@@ -18,5 +20,5 @@ package("fx-gltf")
             void test() {
                 fx::gltf::Document doc;
             }
-        ]]}, {configs = {languages = "c++17"}, includes = "fx/gltf.h"}))
+        ]]}, {configs = {languages = {"c++14", "c++17"}}, includes = "fx/gltf.h"}))
     end)
